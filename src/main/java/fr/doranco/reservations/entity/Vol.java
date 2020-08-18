1df26de67396370a2e4035693b9f776605475bbe
Benoit Técher <techer.benoit2113@gmail.com>
2020-08-18 15:40:20 +0200

    Ajout classe Vol

---
+++ src/main/java/fr/doranco/reservations/entity/Vol.java
@@ -0,0 +1,9 @@
+package fr.doranco.reservations.entity;
+
+public class Vol {
+	private Integer id;
+	private Integer numero;
+	private String type;
+	private String modele;
+	private Integer nbPlaces;
+}
