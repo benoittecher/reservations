1acefe6bcaa62e0e1570718c49708ec2d54963d3
souleymane Diallo <dialau85@yahoo.fr>
2020-08-18 15:05:38 +0200

    ajout des entités

---
+++ src/main/java/fr/doranco/reservations/entity/Reservation.java
@@ -0,0 +1,12 @@
+package fr.doranco.reservations.entity;
+
+import java.util.Date;
+
+public class Reservation {
+
+	private Integer id;
+	private String ref;
+	private Date dateDebut;
+	private Date deteFin;
+	private Float prix;
+}
--- src/main/java/fr/doranco/reservations/entity/User.java
+++ src/main/java/fr/doranco/reservations/entity/User.java
@@ -2,4 +2,117 @@ package fr.doranco.reservations.entity;
 
 public class User {
 
+	private Integer id;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String login;
+	private String password;
+	private String telephone;
+	private Adresse adresse;
+	
+	
+	public User() {
+	}
+
+
+	public User(String nom, String prenom, String email, String login, String password, String telephone) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.login = login;
+		this.password = password;
+		this.telephone = telephone;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getLogin() {
+		return login;
+	}
+
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", email=" + email + ", login=" + login
+				+ ", password=" + password + ", telephone=" + telephone + ", adresse=" + adresse + "]";
+	}
+	
+	
+	
+	
 }
