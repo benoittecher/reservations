ee2dc422d704e6fe3509cdc25aa234345fbfab81
souleymane Diallo <dialau85@yahoo.fr>
2020-08-18 16:37:19 +0200

    ajout classe main

--- src/main/test/Main.java
+++ src/main/test/Main.java
@@ -1,7 +1,21 @@
 
+import org.hibernate.Transaction;
+
+import fr.doranco.reservations.entity.User;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		
+		User user1 = new User("Albert", "Camus","ge@key.com","jay","1234hh","");
+		User user2 = new User("Paul", "Gilbert", "hey@hello.com","hello","HGF11","");
+		
+		Transaction tx = null;
+		
+		
+	
+		System.exit(0);
 
     }
 
