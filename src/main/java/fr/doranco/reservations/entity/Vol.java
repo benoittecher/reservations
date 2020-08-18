50c1647906f1d0adbd86fc2cc4c62bc830ad7657
Benoit Técher <techer.benoit2113@gmail.com>
2020-08-18 16:01:46 +0200

    Ajout Serializable pour Vol

--- src/main/java/fr/doranco/reservations/entity/Vol.java
+++ src/main/java/fr/doranco/reservations/entity/Vol.java
@@ -1,9 +1,45 @@
 package fr.doranco.reservations.entity;
 
-public class Vol {
+import java.io.Serializable;
+
+public class Vol implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private Integer numero;
 	private String type;
 	private String modele;
 	private Integer nbPlaces;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getModele() {
+		return modele;
+	}
+	public void setModele(String modele) {
+		this.modele = modele;
+	}
+	public Integer getNbPlaces() {
+		return nbPlaces;
+	}
+	public void setNbPlaces(Integer nbPlaces) {
+		this.nbPlaces = nbPlaces;
+	}
 }
