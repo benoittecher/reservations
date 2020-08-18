510fdff53d45f1dd81ec5d4345331ff0e0d692ad
souleymane Diallo <dialau85@yahoo.fr>
2020-08-18 16:23:16 +0200

    Creation de la classe UserDAO

---
+++ src/main/java/fr/doranco/reservations/model/dao/UserDao.java
@@ -0,0 +1,43 @@
+package fr.doranco.reservations.model.dao;
+
+import fr.doranco.reservations.entity.User;
+
+public class UserDao implements IUserDao {
+
+	@Override
+	public User getUserById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User addUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removePersonne(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void seConnecter(String login, String password) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void seDeconnecter() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
