55ee681e0958d88947ef109c205ffc215d3a360c
Benoit Técher <techer.benoit2113@gmail.com>
2020-08-18 16:09:03 +0200

    Ajout ReservationDao + interface

---
+++ src/main/java/fr/doranco/reservations/model/dao/ReservationDao.java
@@ -0,0 +1,31 @@
+package fr.doranco.reservations.model.dao;
+
+import fr.doranco.reservations.entity.Reservation;
+
+public class ReservationDao implements IReservationDao{
+
+	@Override
+	public Reservation addReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation updateReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation getReservationById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
