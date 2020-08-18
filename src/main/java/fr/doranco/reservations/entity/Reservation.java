4bcfa0fdced12bd35e32af172cb7ee20f03f1648
souleymane Diallo <dialau85@yahoo.fr>
2020-08-18 15:31:50 +0200

    Ajout de la classe Rservation

--- src/main/java/fr/doranco/reservations/entity/Reservation.java
+++ src/main/java/fr/doranco/reservations/entity/Reservation.java
@@ -9,4 +9,69 @@ public class Reservation {
 	private Date dateDebut;
 	private Date deteFin;
 	private Float prix;
+	
+	
+	public Reservation() {
+	}
+
+
+	public Reservation(String ref, Date dateDebut, Date deteFin, Float prix) {
+		this.ref = ref;
+		this.dateDebut = dateDebut;
+		this.deteFin = deteFin;
+		this.prix = prix;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getRef() {
+		return ref;
+	}
+
+
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+
+
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+
+	public Date getDeteFin() {
+		return deteFin;
+	}
+
+
+	public void setDeteFin(Date deteFin) {
+		this.deteFin = deteFin;
+	}
+
+
+	public Float getPrix() {
+		return prix;
+	}
+
+
+	public void setPrix(Float prix) {
+		this.prix = prix;
+	}
+	
+	
+	
 }
